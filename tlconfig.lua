6001ff9f642c0e40ae93f5406268a92fd73079d9
b0ryakha <tosha.mar4encko@yandex.ru>
2024-08-20 17:37:14 +0700

    Add more lvl variation.

--- tlconfig.lua
+++ tlconfig.lua
@@ -1,5 +1,5 @@
 return {
-    global_env_def = "externals/moonshine/moonshine",
+    global_env_def = "externals/moonshine/declarations",
     gen_target = "5.1",
     skip_compat53 = true,
     include_dir = {
